932b28a15a5707135e7095950000a838db409511
Anton Yefremov <anton.yefremov@gmail.com>
2017-04-27 22:39:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,11 @@
 module.exports = {
   average: require('./average'),
   count: require('./count'),
+  deviation: require('./deviation'),
   max: require('./max'),
+  median: require('./median'),
   min: require('./min'),
   percentile: require('./percentile'),
-  sum: require('./sum')
+  sum: require('./sum'),
+  variance: require('./variance')
 };
